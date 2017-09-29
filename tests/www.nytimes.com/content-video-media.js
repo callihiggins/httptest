9d750b07bf0e0df9d79ed0cbc22fd73741c4b5e1
fsouza <fsouza@users.noreply.github.com>
2017-09-29 09:50:14 -0400

    Fix video-media HLS segment test (#395)
    The URL of HLS segments has changed. And they also include a surrogate
    key indicating that they were generated by the on-the-fly video
    packager.

--- tests/www.nytimes.com/content-video-media.js
+++ tests/www.nytimes.com/content-video-media.js
@@ -24,7 +24,7 @@ function getScenarioEvents()
       'responseHeaderMatches': {
         'x-frame-options': 'DENY',
         'content-type': 'application/x-javascript',
-        'x-api-version': 'F-VM',        
+        'x-api-version': 'F-VM',
       },
       'responseHeadersPresent': [
         'age',
@@ -44,7 +44,7 @@ function getScenarioEvents()
       'scenarioDescription': 'Test /video-media routing',
       'timestamp': 1,
       'testId': 1,
-    },  
+    },
     {
       'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
       'isDeployedInEnv': {
@@ -57,11 +57,11 @@ function getScenarioEvents()
         'Fastly-Debug': '1',
       },
       'requestScheme': 'https://',
-      'requestUri': '/video-media/2017/03/27/38483_1_arconic-fiction-video_v1_wg_hls/240p_hls/video-00002.ts',
+      'requestUri': '/video-media/2017/03/27/38483_1_arconic-fiction-video_v1_wg_hls/segment-2-f2-v1-a1.ts',
       'responseHeaderMatches': {
         'x-frame-options': 'DENY',
         'content-type': 'video/mp2t',
-        'surrogate-key': 'video/38483',
+        'surrogate-key': 'video/38483 video/chop-chop',
         'x-api-version': 'F-VM',
       },
       'responseHeadersPresent': [
@@ -82,7 +82,7 @@ function getScenarioEvents()
       'scenarioDescription': 'Test /video-media routing',
       'timestamp': 1,
       'testId': 2,
-    },  
+    },
   ];
 
   return scenarios;
