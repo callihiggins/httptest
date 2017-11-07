e8f57228bbc2e44daa4e6a3004af905bc3275397
Shawn Gao <shawn.gao@nytimes.com>
2017-11-07 18:01:02 -0500

    update test with header pattern match for both article backends; remo… (#468)
    * update test with header pattern match for both article backends; remove test sandbox symlink
    
    * update test to separate header pattern and header match
    
    * correct typo
    
    * update regex to be more precise

--- tests/www.nytimes.com/content-article.js
+++ tests/www.nytimes.com/content-article.js
@@ -11,33 +11,37 @@ function getScenarioEvents()
     {
       'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
       'isDeployedInEnv': {
-        'prd': false,
+        'prd': true,
         'stg': true,
         'dev': false,
         'sbx': false,
       },
       'requestScheme': 'https://',
       'requestUri': '/2016/11/04/homepage/new-york-times-open-access-election-2016.html',
+      'responseHeaderPattern': {
+        'x-api-version': /F-(GA|5-5)/,
+      },
       'responseHeaderMatches': {
-        'x-api-version': 'F-GA',
         'x-pagetype': 'article',
       },
-      'responseStatusCode': [200],
+      'responseStatusCode': [200,404],
       'scenarioDescription': 'Test article, /2016/11/04/homepage/new-york-times-open-access-election-2016.html',
       'testId': 1,
     },
     {
       'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
       'isDeployedInEnv': {
-        'prd': false,
+        'prd': true,
         'stg': true,
         'dev': false,
         'sbx': false,
       },
       'requestScheme': 'http://',
       'requestUri': '/2004/01/09/us/2004-campaign-fund-raising-financial-firms-are-bush-s-biggest-donors-study.html',
+      'responseHeaderPattern': {
+        'x-api-version': /F-(GA|5-5)/,
+      },
       'responseHeaderMatches': {
-        'x-api-version': 'F-GA',
         'x-pagetype': 'article',
       },
       'responseStatusCode': [200],
@@ -47,15 +51,17 @@ function getScenarioEvents()
     {
       'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
       'isDeployedInEnv': {
-        'prd': false,
+        'prd': true,
         'stg': true,
         'dev': false,
         'sbx': false,
       },
       'requestScheme': 'http://',
       'requestUri': '/2005/03/01/business/worldbusiness/chinas-oil-diplomacy-in-latin-america.html',
+      'responseHeaderPattern': {
+        'x-api-version': /F-(GA|5-5)/,
+      },
       'responseHeaderMatches': {
-        'x-api-version': 'F-GA',
         'x-pagetype': 'article',
       },
       'responseStatusCode': [200],
@@ -65,15 +71,17 @@ function getScenarioEvents()
     {
       'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
       'isDeployedInEnv': {
-        'prd': false,
+        'prd': true,
         'stg': true,
         'dev': false,
         'sbx': false,
       },
       'requestScheme': 'http://',
       'requestUri': '/1959/01/11/archives/sports-of-the-times-forecast-for-1959.html',
+      'responseHeaderPattern': {
+        'x-api-version': /F-(GA|5-5)/,
+      },
       'responseHeaderMatches': {
-        'x-api-version': 'F-GA',
         'x-pagetype': 'article',
       },
       'responseStatusCode': [200],
@@ -83,15 +91,17 @@ function getScenarioEvents()
     {
       'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
       'isDeployedInEnv': {
-        'prd': false,
+        'prd': true,
         'stg': true,
         'dev': false,
         'sbx': false,
       },
       'requestScheme': 'http://',
       'requestUri': '/1964/01/01/mexico-tightens-control-of-semipublic-concerns.html',
+      'responseHeaderPattern': {
+        'x-api-version': /F-(GA|5-5)/,
+      },
       'responseHeaderMatches': {
-        'x-api-version': 'F-GA',
         'x-pagetype': 'article',
       },
       'responseStatusCode': [200],
@@ -101,15 +111,17 @@ function getScenarioEvents()
     {
       'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
       'isDeployedInEnv': {
-        'prd': false,
+        'prd': true,
         'stg': true,
         'dev': false,
         'sbx': false,
       },
       'requestScheme': 'http://',
       'requestUri': '/1979/01/01/archives/carolyn-spiro-married-to-gregory-j-cannata.html',
+      'responseHeaderPattern': {
+        'x-api-version': /F-(GA|5-5)/,
+      },
       'responseHeaderMatches': {
-        'x-api-version': 'F-GA',
         'x-pagetype': 'article',
       },
       'responseStatusCode': [200],
@@ -119,15 +131,17 @@ function getScenarioEvents()
     {
       'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
       'isDeployedInEnv': {
-        'prd': false,
+        'prd': true,
         'stg': true,
         'dev': false,
         'sbx': false,
       },
       'requestScheme': 'http://',
       'requestUri': '/2001/01/20/technology/20ANNIVERSARY.html',
+      'responseHeaderPattern': {
+        'x-api-version': /F-(GA|5-5)/,
+      },
       'responseHeaderMatches': {
-        'x-api-version': 'F-GA',
         'x-pagetype': 'article',
       },
       'responseStatusCode': [301],
@@ -137,15 +151,17 @@ function getScenarioEvents()
     {
       'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
       'isDeployedInEnv': {
-        'prd': false,
+        'prd': true,
         'stg': true,
         'dev': false,
         'sbx': false,
       },
       'requestScheme': 'http://',
       'requestUri': '/2001/01/20/technology/the-new-york-times-five-years-on-the-web.html',
+      'responseHeaderPattern': {
+        'x-api-version': /F-(GA|5-5)/,
+      },
       'responseHeaderMatches': {
-        'x-api-version': 'F-GA',
         'x-pagetype': 'article',
       },
       'responseStatusCode': [200],
@@ -155,15 +171,17 @@ function getScenarioEvents()
     {
       'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
       'isDeployedInEnv': {
-        'prd': false,
+        'prd': true,
         'stg': true,
         'dev': false,
         'sbx': false,
       },
       'requestScheme': 'http://',
       'requestUri': '/2006/01/29/fashion/sundaystyles/29LOVE.html',
+      'responseHeaderPattern': {
+        'x-api-version': /F-(GA|5-5)/,
+      },
       'responseHeaderMatches': {
-        'x-api-version': 'F-GA',
         'x-pagetype': 'article',
       },
       'responseStatusCode': [200],
@@ -173,15 +191,17 @@ function getScenarioEvents()
     {
       'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
       'isDeployedInEnv': {
-        'prd': false,
+        'prd': true,
         'stg': true,
         'dev': false,
         'sbx': false,
       },
       'requestScheme': 'http://',
       'requestUri': '/2006/02/26/fashion/sundaystyles/26LOVE.html',
+      'responseHeaderPattern': {
+        'x-api-version': /F-(GA|5-5)/,
+      },
       'responseHeaderMatches': {
-        'x-api-version': 'F-GA',
         'x-pagetype': 'article',
       },
       'responseStatusCode': [200],
@@ -191,15 +211,17 @@ function getScenarioEvents()
     {
       'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
       'isDeployedInEnv': {
-        'prd': false,
+        'prd': true,
         'stg': true,
         'dev': false,
         'sbx': false,
       },
       'requestScheme': 'http://',
       'requestUri': '/2006/11/12/fashion/12love.html',
+      'responseHeaderPattern': {
+        'x-api-version': /F-(GA|5-5)/,
+      },
       'responseHeaderMatches': {
-        'x-api-version': 'F-GA',
         'x-pagetype': 'article',
       },
       'responseStatusCode': [200],
@@ -209,15 +231,17 @@ function getScenarioEvents()
     {
       'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
       'isDeployedInEnv': {
-        'prd': false,
+        'prd': true,
         'stg': true,
         'dev': false,
         'sbx': false,
       },
       'requestScheme': 'http://',
       'requestUri': '/blog/2000/01/01/us/slug.html',
+      'responseHeaderPattern': {
+        'x-api-version': /F-(GA|5-5)/,
+      },
       'responseHeaderMatches': {
-        'x-api-version': 'F-GA',
         'x-pagetype': 'article',
       },
       'responseStatusCode': [404],
