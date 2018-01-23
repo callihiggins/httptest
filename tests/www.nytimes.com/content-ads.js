5a13e281bce5ddb9bdae750303cbfaabc566fde6
Price <carolyn.price@nytimes.com>
2018-01-23 11:52:50 -0500

    adds ads test

---
+++ tests/www.nytimes.com/content-ads.js
@@ -0,0 +1,31 @@
+var suite = require("/lib/suite.js");
+var scenarios = getScenarioEvents();
+suite.run(suite, scenarios);
+
+/**
+ * @return array
+ */
+function getScenarioEvents() {
+  var scenarios = [
+    {
+      id: "FunctionalTestScenarioDefinitionForAdsGCSBucket",
+      isDeployedInEnv: {
+        prd: false,
+        stg: false,
+        dev: true,
+        sbx: false
+      },
+      requestScheme: "https://",
+      requestUri: "/ads/healthcheck.txt",
+      responseHeaderMatches: {
+        "x-pagetype": "ads-static-assets"
+      },
+      responseHeadersPresent: ["x-goog-generation","x-goog-hash","x-goog-metageneration","x-goog-storage-class","x-guploader-uploadid"],
+      responseStatusCode: [200],
+      scenarioDescription: "Ads GCS bucket responds with 200",
+      testId: 1
+    },
+  ];
+
+  return scenarios;
+}
