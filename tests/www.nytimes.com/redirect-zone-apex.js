0d438f151cf62e67934cf5fc058ccb08427c160b
Jon Oden <jonoden@users.noreply.github.com>
2018-02-12 10:49:15 -0500

    clean up the zone apex redirect (#639)
    * remove cookie from requests to cms static assets
    
    * overriding cache control to test caching cms assets
    
    * typo
    
    * test was successful, remove code to override origin cache-control
    
    * fix nytimes.com -> www.nytimes.com redirect
    
    * add x-api-version 0 to denote no origin, add redir reason so it can be logged
    
    * add a test for the zone apex redirect
    
    * change object ref

---
+++ tests/www.nytimes.com/redirect-zone-apex.js
@@ -0,0 +1,35 @@
+var suite = require('/lib/suite.js');
+var scenarios = getScenarioEvents();
+suite.run(suite, scenarios);
+
+/**
+ * @return array
+ */
+function getScenarioEvents()
+{
+  var scenarios = [
+    {
+      'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
+      'isDeployedInEnv': {
+        'prd': true,
+        'stg': false,
+        'dev': false,
+        'sbx': false,
+      },
+      'requestHeaders': {
+        'Host': 'nytimes.com',
+      },
+      'requestScheme': 'https://',
+      'requestUri': '/',
+      'responseHeaderMatches': {
+        'x-api-version': 'F-0',
+        'location': 'https://www.nytimes.com/',
+      },
+      'responseStatusCode': 301,
+      'scenarioDescription': 'redirect-zone-apex: Test zone apex redirects to www.nytimes.com',
+      'testId': 1,
+    },
+  ];
+
+  return scenarios;
+}
