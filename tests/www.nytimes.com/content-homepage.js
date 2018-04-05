4af7714d93e1e52b6af90813c4b4b48cde6de430
Adam E. Falk <falkae@nytimes.com>
2018-04-05 10:34:43 -0400

    remove obsolete test scenario for Homepage (#783)

--- tests/www.nytimes.com/content-homepage.js
+++ tests/www.nytimes.com/content-homepage.js
@@ -27,28 +27,6 @@ function getScenarioEvents()
       'scenarioDescription': 'Test homepage vi_www_hp_opt is set to 0',
       'testId': 1,
     },
-    {
-      'id': 'FunctionalTestScenarioDefinitionForHtmlPage',
-      'isDeployedInEnv': {
-        'prd': true,
-        'stg': true,
-        'dev': true,
-        'sbx': false,
-      },
-      'requestHeaderCookie': [
-        'nyt.dv.nyt5-on-gke=1',
-        'vi_www_hp_opt=0'
-      ],
-      'requestScheme': 'https://',
-      'requestUri': '/?abra=WP_ProjectVi_www_hp=0',
-      'responseHeaderMatches': {
-        'x-pagetype': 'homepage',
-        'x-nyt-backend': 'homepage_fe',
-      },
-      'responseStatusCode': 200,
-      'scenarioDescription': 'Test homepage vi_www_hp_opt is set to 0, nyt5-on-gke is set to  1',
-      'testId': 101,
-    },
   ];
 
   return scenarios;
