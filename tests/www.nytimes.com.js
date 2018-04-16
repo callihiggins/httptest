2b30dfd820faca95c0b06f530e7da8ecaa160e24
Carrie Price <ccpricenytimes@users.noreply.github.com>
2018-04-16 19:03:01 -0400

    remove testfile that nolonger exists (#815)

--- tests/www.nytimes.com.js
+++ tests/www.nytimes.com.js
@@ -30,7 +30,6 @@ require("./www.nytimes.com/route-times-journeys.js");
 require("./www.nytimes.com/vi-allocation.js");
 require("./www.nytimes.com/vi-assets.js");
 require('./www.nytimes.com/vi-story.js');
-require('./www.nytimes.com/vi-apps.js');
 require("./www.nytimes.com/route-programs.js");
 require("./www.nytimes.com/route-adx.js");
 require("./www.nytimes.com/route-video.js");
