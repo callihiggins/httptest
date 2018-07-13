63f003de018d6a31ca1c6c9459666b2bf1eb8b17
Jon Oden <jonoden@users.noreply.github.com>
2018-07-13 15:23:04 -0400

    remove obsolete sbx env from test suite (#987)

--- tests/www.nytimes.com/route-homepage.js
+++ tests/www.nytimes.com/route-homepage.js
@@ -14,7 +14,6 @@ function getScenarioEvents()
         'prd': true,
         'stg': true,
         'dev': true,
-        'sbx': true,
       },
       'requestHeaders': {
         'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36 (NodeJS test suite <falkae@nytimes.com>)',
@@ -36,7 +35,6 @@ function getScenarioEvents()
         'prd': true,
         'stg': true,
         'dev': true,
-        'sbx': true,
       },
       'requestHeaders': {
         'User-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25 (NodeJS test suite <falkae@nytimes.com>)',
