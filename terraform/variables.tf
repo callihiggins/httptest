738ee1ca1fb0b6a2820def96f3ec8361912e1cc8
Shawn Gao <shawn.gao@nytimes.com>
2018-11-14 15:33:37 -0500

    new sumo collector with json log format per DV-1709 (#1091)
    * test provider version change
    
    * update sumo config
    
    * make one sumo attr change with provider 0.1.3
    
    * bump provider to 0.4.0
    
    * update additional sumo log attribute
    
    * bump provider 0.4.0
    
    * tuned down provider to 0.2.0
    
    * bumper provider again 0.4.0
    
    * update sumo attr, aka field name
    
    * test log purge with json format
    
    * set purge logger and re-try purge log
    
    * missing semicolon
    
    * long string format
    
    * removing the format part of purge logging
    
    * retry with correct variable
    
    * make certain the build still working
    
    * purge logger in 2nd sumo intergration
    
    * 2nd logger for purge
    
    * logging purge in json format
    
    * adding the 3rd sumo intergration
    
    * still need log line for json
    
    * long string format
    
    * semicolon at the end of log line
    
    * json format
    
    * stupid closing parathesis
    
    * remore sumo purge log intergration as it is not working
    
    * adding debug response header
    
    * reformat json logline
    
    * remove quote for a proper formated json
    
    * re-format json string
    
    * re-adjust json format
    
    * semi-comma vs comma format
    
    * json format
    
    * adjust json
    
    * another update on json
    
    * update json
    
    * update json
    
    * json update
    
    * update json
    
    * update json
    
    * adding new, rename certain fields
    
    * removing debug header
    
    * log field name update
    
    * remove dup condition and update logger name reference in log_purge
    
    * group sumo with format data injection from yml file
    
    * update format
    
    * add escape sequence
    
    * quote for version number
    
    * update yml format
    
    * escape one more
    
    * log 100% of status code 400 and above

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,6 +11,15 @@ variable "service_name" {
   type = "string"
 }
 
+variable "sumo_logger_name" {
+  type = "map"
+  default = {
+  dev = "fastly-www/stg"
+  stg = "fastly-www/stg"
+  prd = "fastly-www/prd"
+  }
+}
+
 variable "sumologic_endpoint" {
   type = "map"
 
@@ -18,6 +27,15 @@ variable "sumologic_endpoint" {
     dev = "https://collectors.sumologic.com/receiver/v1/http/ZaVnC4dhaV090sPFU_YnnEP_F61BBPpciroz627Cc5zoPONnZQtGWWIiqPAs6g3LsPXOc3JfFUPl44yl-tlLnkH-Pn7xp1X9QarWeCQeQjSPeA0wqbLn-A=="
     stg = "https://collectors.sumologic.com/receiver/v1/http/ZaVnC4dhaV090sPFU_YnnEP_F61BBPpciroz627Cc5zoPONnZQtGWWIiqPAs6g3LsPXOc3JfFUPl44yl-tlLnkH-Pn7xp1X9QarWeCQeQjSPeA0wqbLn-A=="
     prd = "https://collectors.sumologic.com/receiver/v1/http/ZaVnC4dhaV3DIdcy2GqnQ5DMhk6CZkjlLK9C5V5fjRSlaVm4RJg3dU1REHf-_owjetrn1mzlLqDx1KlNQ6qwtrgHf1KwvOaVcZBHpQSdqG-tFYf0OFYEUQ=="
+  }
+}
+
+variable "sumologic_endpoint_json" {
+  type = "map"
+  default = {
+    dev = "https://collectors.sumologic.com/receiver/v1/http/ZaVnC4dhaV2LlpGb4VDoidBmH_To3weFMf_Eo8ujQDiiACqhhpxfn4-GTfWTFdSah4oLID8jTZ2mCP_0cqCbVU7qePWuBJoxDdIIwDgm2SFZL2g30zal5Q=="
+    stg = "https://collectors.sumologic.com/receiver/v1/http/ZaVnC4dhaV2LlpGb4VDoidBmH_To3weFMf_Eo8ujQDiiACqhhpxfn4-GTfWTFdSah4oLID8jTZ2mCP_0cqCbVU7qePWuBJoxDdIIwDgm2SFZL2g30zal5Q=="
+    prd = "https://collectors.sumologic.com/receiver/v1/http/ZaVnC4dhaV0fRib17FqHM8YTjuKyp-s-Cb1QEdsTH3fvZnEGVg2187Ed7SbAvyBFOwz2-GEtrp_5Oldnmt2toYlq4jVtgfHEnXsLdiOh-O3r2H4XaqtEXw=="
   }
 }
 
