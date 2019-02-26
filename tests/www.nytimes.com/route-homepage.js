a926f9b6d7be21e703eba55fe4a6197e98f39974
Daniel Kao <dkao@diplateevo.com>
2019-02-26 13:04:13 -0800

    Remove Comments Test (#1196)

--- tests/www.nytimes.com/route-homepage.js
+++ tests/www.nytimes.com/route-homepage.js
@@ -22,40 +22,6 @@ function getScenarioEvents()
       'scenarioDescription': 'POST method requests are not allowed for homepage route',
       'testId': 220
     },
-
-    // generate a bunch of nyt-a's like this (in bash):
-
-    /*
-    for i in {1..1000};
-    do a="$(base64 /dev/random | head -c22 | tr /+ -_)";
-    s="$a HOME_comments_count"; # Put your ABRA test name here
-    st=$(printf %s "$s" | openssl dgst -sha256 | cut -c1-8);
-    printf '%s %010d\n' "$s" $((0x$st));
-    done
-    */
-
-    {
-      'scenarioDescription': 'Request with nyt-a mapping to HOME_comments_count allocation 0_control, 0-33%',
-      'isDeployedInEnv': {
-        'prd': true,
-        'stg': true,
-        'dev': true
-      },
-      'requestHeaders': {
-        'x-nyt-debug': '1',
-      },
-      'requestHeaderCookie': [
-        // var.d = 0617867856
-        'nyt-a=I0d9m-PDuak4gBZaZhWQCq',
-      ],
-      'requestScheme': 'https://',
-      'requestUri': '/',
-      'responseHeaderContains': {
-        'x-nyt-debug-req-http-x-nyt-vi-abtest': 'HOME_comments_count=0_control',
-      },
-      'responseStatusCode': 200,
-      'testId': 290,
-    },
   ];
 
   return scenarios;
